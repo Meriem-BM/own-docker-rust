fd2a8f19cf2663e22e3c8213a81de3410dec76a9
Meriem-BM <barhoumi.meriem1@gmail.com>
2023-10-07 15:40:45 +0100

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,25 @@ fn main() -> Result<()> {
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage!
-    // let args: Vec<_> = std::env::args().collect();
-    // let command = &args[3];
-    // let command_args = &args[4..];
-    // let output = std::process::Command::new(command)
-    //     .args(command_args)
-    //     .output()
-    //     .with_context(|| {
-    //         format!(
-    //             "Tried to run '{}' with arguments {:?}",
-    //             command, command_args
-    //         )
-    //     })?;
-    //
-    // if output.status.success() {
-    //     let std_out = std::str::from_utf8(&output.stdout)?;
-    //     println!("{}", std_out);
-    // } else {
-    //     std::process::exit(1);
-    // }
+    let args: Vec<_> = std::env::args().collect();
+    let command = &args[3];
+    let command_args = &args[4..];
+    let output = std::process::Command::new(command)
+        .args(command_args)
+        .output()
+        .with_context(|| {
+            format!(
+                "Tried to run '{}' with arguments {:?}",
+                command, command_args
+            )
+        })?;
+    
+    if output.status.success() {
+        let std_out = std::str::from_utf8(&output.stdout)?;
+        println!("{}", std_out);
+    } else {
+        std::process::exit(1);
+    }
 
     Ok(())
 }
